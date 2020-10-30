8d33c54e807ae6eefc25c4aa1a176bfea398d00d
Daniel <jdaniel0206@hotmail.com>
2020-10-30 14:55:46 -0600

    123
    123

--- UberApp/src/main/java/org/una/controllers/AcercaDeController.java
+++ UberApp/src/main/java/org/una/controllers/AcercaDeController.java
@@ -1,7 +1,11 @@
 package org.una.controllers;
 
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 public class AcercaDeController {
 
 
-
+    public AnchorPane acp_acercade;
+    public ImageView img_fondo;
 }
--- UberApp/src/main/java/org/una/controllers/InstruccionesController.java
+++ UberApp/src/main/java/org/una/controllers/InstruccionesController.java
@@ -1,7 +1,10 @@
 package org.una.controllers;
 
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class InstruccionesController {
     public Label lbl_titulo;
+    public AnchorPane acp_instrucciones;
+    public Label lbl_instrucciones;
 }
